a9a4612d960acbd5d4f1a2070b31177bf3d069d5
Thomas Bender <thomasmatthewbender@gmail.com>
2022-03-21 12:52:34 -0400

    commit tree 2a9247219c85af114caab6c153157d37472ac8d4

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+
+
+function writeCards(name, event) {
+    const newArray = []
+    for (let i = 0; i < name.length; i++) {
+    newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArray
+}
+
+
+
+
+
+
+
+function countDown() {
+    let countDowns = 10;
+    while (countDowns <= 10 && countDowns >= 0) {
+      console.log(countDowns--);
+    }
+}
